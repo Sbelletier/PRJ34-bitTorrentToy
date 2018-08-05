6c771fae29492337bdc69c8636ea6167bf332f46
Sbelletier <sbelletier@gmail.com>
2018-08-05 16:51:07 +0200

    Bencoding Done

---
+++ bencoding.py
@@ -0,0 +1,158 @@
+#-*- coding:utf-8 -*-
+"""
+Ce fichier contient les fonctions utilitaires
+pour gerer l'encodage bencoding
+"""
+
+
+
+def getEncodedInt( string ):
+    """
+    Renvoie l'entier dont l'encodage commence a string[0],
+    et une version modifiée de string d'ou l'encodage de
+    l'entier lu a été consommé
+
+    Hypothèse de départ :
+    string[0] == 'i'
+    """
+    string = string[1:] #On enleve le premier i
+    intString = ""#Chaine contenant l'entier en lui même
+    #Lecture de l'entier
+    while( string[0] != 'e'):
+        intString += string[0]
+        string = string[1:]
+    #Conversion
+    integer = int( intString )
+    #Retour
+    string = string[1:] #on enleve le e final
+    return integer, string
+
+
+def getEncodedStr( string ):
+    """
+    Renvoie la chaine de caracteres dont l'encodage commence a string[0],
+    et une version modifiée de string d'ou l'encodage de la chaine lue
+    a été consommé
+
+    Hypothèse de départ :
+    string[0] in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+    """
+    lBuffer = ""
+    #Lecture de la longueur
+    while( string[0] != ':'):
+        lBuffer += string[0]
+        string = string[1:]
+    string = string[1:] #On retire le : de separation
+    #Conversion de la longueur
+    stringLength = int( lBuffer )
+    #Lecture de la chaine
+    returnString = ""
+    for i in range( stringLength ):
+        returnString += string[0]
+        string = string[1:]
+    #Retour
+    return returnString, string
+
+
+def getEncodedList( string ):
+    """
+    Renvoie la liste dont l'encodage commence a string[0],
+    et une version modifiée de string d'ou l'encodage de 
+    la liste lue a été consommé
+
+    Hypothèse de départ :
+    string[0] == 'l'
+    """
+    string = string[1:]#On enleve le l initial
+    bencodedList = []
+    #Lecture de la liste
+    while( string[0] != 'e' ):
+        content = None
+        #Switch sur les 4 elements possibles
+        if( string[0] == 'l' ):
+            content, string = getEncodedList( string )
+        elif( string[0] == 'd' ):
+            content, string = getEncodedDict( string )
+        elif( string[0] == 'i'):
+            content, string = getEncodedInt( string )
+        elif( string[0] in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') ):
+            content, string = getEncodedStr( string )
+        #On append le nouveau contenu
+        bencodedList.append(content)
+    #Retour
+    string = string[1:] #On enleve le e final
+    return bencodedList, string
+
+
+def getEncodedDict( string ):
+    """
+    Renvoie le dictionnaire dont l'encodage commence a string[0],
+    et une version modifiée de dictionnaire d'ou l'encodage du 
+    dictionnaire lue a été consommé
+
+    Hypothèse de départ :
+    string[0] == 'd'
+    """
+    string = string[1:]#On enleve le l initial
+    bencodedDict = dict()
+    while( string[0] != 'e' ):
+        #On lit la clé
+        key, string = getEncodedStr( string )
+        #On lit l'element indexé
+        content = None
+        #Switch sur les 4 elements possibles
+        if( string[0] == 'l' ):
+            content, string = getEncodedList( string )
+        elif( string[0] == 'd' ):
+            content, string = getEncodedDict( string )
+        elif( string[0] == 'i'):
+            content, string = getEncodedInt( string )
+        elif( string[0] in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') ):
+            content, string = getEncodedStr( string )
+        #On append le nouveau contenu
+        bencodedDict[key] = content
+    #Retour
+    string = string[1:] #On enleve le e final
+    return bencodedDict, string
+
+
+def getEncodedObject( string ):
+    """
+    Renvoie l'element encodé et son type en tant que string
+
+    Renvoie None, "error" en cas d'erreur de lecture
+    """
+    #Switch sur les 4 elements possibles
+    if( string[0] == 'l' ):
+        content, string = getEncodedList( string )
+        return content, "list"
+    elif( string[0] == 'd' ):
+        content, string = getEncodedDict( string )
+        return content, "dict"
+    elif( string[0] == 'i'):
+        content, string = getEncodedInt( string )
+        return content, "int"
+    elif( string[0] in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') ):
+        content, string = getEncodedStr( string )
+        return content, "str"
+    return None, "error"
+
+"""
+Petit Test:
+Encodage de "ll12:constitutioni35ee10:sauvegardei9ed3:bar4:spam3:fooi42eee"
+Retour Attendu : "[ 
+                    [ 'constitution', 35 ], 
+                    'sauvegarde', 
+                    9, 
+                    {'foo':42, 'bar':'spam'}    
+                ]"
+"""
+if __name__ == '__main__':
+    string = "ll12:constitutioni35ee10:sauvegardei9ed3:bar4:spam3:fooi42eee"
+    print "initial bencoding"
+    print string
+    print "first decoding"
+    result, typeObject = getEncodedObject( string )
+    print result
+    print typeObject
+    
