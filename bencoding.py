8b5b5f25ce8891d1cccd3db9b1be7ee86154354e
Sbelletier <sbelletier@gmail.com>
2018-08-22 15:41:00 +0200

    enforce consistent order for encoding of dictionaries

--- bencoding.py
+++ bencoding.py
@@ -149,7 +149,8 @@ def getEncodedDict( dico ):
     Renvoie la chaine encodant le dictionnaire pris en paramètre
     """
     string = "d"
-    for key in dico.keys():
+    keys = dico.keys().sort()
+    for key in keys:
         string += getEncodedString(key)
         string += getEncodedObject( dico[key] )
     string += "e"
